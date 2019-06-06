483055ea68a8d46ed5ba7e932fbde03f94df4480
dstarrtrey <dstarrtrey@gmail.com>
2019-06-06 11:43:01 -0700

    Changes backend endpoint in config

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
 export const devEndpoint = `http://localhost:4000`
-export const prodEndpoint = `https://foodq-yoga-prod.herokuapp.com/`;
+export const prodEndpoint = `https://food-q-backend-prod.herokuapp.com/`;
